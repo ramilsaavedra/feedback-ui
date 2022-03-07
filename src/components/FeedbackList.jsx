1307bad1e7f5aed01786d99813ca429f69b3ceeb
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-07 10:19:06 +0800

    add string as a proptypes for id

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -22,7 +22,10 @@ function FeedbackList({feedback, handleDelete}) {
 FeedbackList.propTypes = {
     feedback: PropTypes.arrayOf(
         PropTypes.shape({
-            id: PropTypes.number.isRequired,
+            id: PropTypes.oneOfType([
+                PropTypes.string.isRequired,
+                PropTypes.number.isRequired
+            ]),
             text: PropTypes.string.isRequired,
             rating: PropTypes.number.isRequired
         })
