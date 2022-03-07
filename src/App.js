c819bb7b296713c6dbe863066e8e33c1fbe2e1ab
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-07 10:37:40 +0800

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import FeedbackData from './data/FeedbackData'
+import AboutPage from './pages/AboutPage'
 
 function App() {
 
@@ -22,14 +24,17 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
             <Header />
             <div className="container">
-                <FeedbackForm handleAdd={addFeedback} />
-                <FeedbackStats feedback={feedback} />  
-                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                <Route path='/' exact> 
+                    <FeedbackForm handleAdd={addFeedback} />
+                    <FeedbackStats feedback={feedback} />  
+                    <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+                </Route>
+                <Route path='/about' component={AboutPage} />
             </div>
-        </>
+        </Router>
 
     )
 }
