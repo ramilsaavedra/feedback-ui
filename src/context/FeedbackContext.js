8412d14b6fa741e30ae5da950094f39dc39a805f
Ramil Saavedra <ramilsmcl@gmail.com>
2022-03-08 10:22:47 +0800

    moved functions on context

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,13 +1,27 @@
 import { createContext, useState } from "react"
 import FeedbackData from "../data/FeedbackData"
+import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
     const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback])
+    }
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
     
     return <FeedbackContext.Provider value={{
         feedback,
+        deleteFeedback,
+        addFeedback
     }}>
         {children}
     </FeedbackContext.Provider>
